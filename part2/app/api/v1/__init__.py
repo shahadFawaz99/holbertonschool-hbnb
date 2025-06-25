e2de10c56e873e4dfbb2be94556aa7857919a863
donnasaud <donnaalmadani@gmail.com>
2025-06-25 17:45:28 +0000

    update init file in task 5

--- part2/app/api/v1/__init__.py
+++ part2/app/api/v1/__init__.py
@@ -1,18 +1,20 @@
-from flask import Blueprint
 from flask_restx import Api
-from api.v1.places import api as places_ns
+from flask import Blueprint
 
-# Create a Flask Blueprint for versioned API routing
-api_bp = Blueprint('api', __name__, url_prefix='/api/v1')
+# Import all namespaces
+from .users import api as user_ns
+from .amenities import api as amenity_ns
+from .places import api as place_ns
+from .reviews import api as review_ns
 
-# Create the API object
-api = Api(
-    api_bp,
-    version='1.0',
-    title='HBnB API',
-    description='API documentation for the HBnB application (Task 4 - Place endpoints)',
-)
+# Create blueprint
+api_v1_bp = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+api = Api(api_v1_bp, version='1.0', title='HBnB API',
+          description='API for the HBnB application')
 
-# Register the namespace for places
-api.add_namespace(places_ns, path='/places')
+# Register namespaces
+api.add_namespace(user_ns, path='/users')
+api.add_namespace(amenity_ns, path='/amenities')
+api.add_namespace(place_ns, path='/places')
+api.add_namespace(review_ns, path='/reviews')
 
