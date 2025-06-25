baf81f46eb08b37f9323469f63cb07e858eca5da
donnasaud <donnaalmadani@gmail.com>
2025-06-25 16:57:15 +0000

    edit facade file

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -30,12 +30,7 @@ class HBnBFacade:
         user = self.user_repo.get(user_id)
         if not user:
             return None
-        for key, value in user_data.items():
-            if hasattr(user, key):
-                setattr(user, key, value)
-        user = self.user_repo.get(user_id)
-        if not user:
-            return None
+
         user.validate()
         if 'first_name' in user_data:
             user.first_name = user_data['first_name']
@@ -52,30 +47,85 @@ class HBnBFacade:
         else:
             raise TypeError('email must be in payload')
 
-        self.user_repo.update(user_id, user_data)
+        for key, value in user_data.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+
+        self.user_repo.update(user_id, user)
         return user
-
-    def get_place(self, place_id):
-        return self.place_repo.get(place_id)
-
-    def get_all_places(self):
-        return self.place_repo.get_all()
 
     def create_place(self, place_data):
         owner = self.user_repo.get(place_data['owner_id'])
         if not owner:
             raise ValueError("Invalid owner_id")
+
+        for amenity_id in place_data.get("amenities", []):
+            if not self.amenity_repo.get(amenity_id):
+                raise ValueError(f"Amenity {amenity_id} does not exist")
+
         place = Place(**place_data)
         self.place_repo.add(place)
         return place
+
+    def get_place(self, place_id):
+        place = self.place_repo.get(place_id)
+        if not place:
+            return None
+
+        owner = self.user_repo.get(place.owner_id)
+        amenities = [self.amenity_repo.get(aid) for aid in place.amenities]
+
+        return {
+            "id": place.id,
+            "title": place.title,
+            "description": place.description,
+            "price": place.price,
+            "latitude": place.latitude,
+            "longitude": place.longitude,
+            "owner": {
+                "id": owner.id,
+                "first_name": owner.first_name,
+                "last_name": owner.last_name,
+                "email": owner.email
+            } if owner else None,
+            "amenities": [
+                {"id": a.id, "name": a.name} for a in amenities if a is not None
+            ]
+        }
+
+    def get_all_places(self):
+        return [
+            {
+                "id": p.id,
+                "title": p.title,
+                "latitude": p.latitude,
+                "longitude": p.longitude
+            } for p in self.place_repo.get_all()
+        ]
 
     def update_place(self, place_id, place_data):
         place = self.place_repo.get(place_id)
         if not place:
             return None
+
+        if "price" in place_data and place_data["price"] < 0:
+            raise ValueError("Price must be non-negative")
+
+        if "latitude" in place_data and not (-90 <= place_data["latitude"] <= 90):
+            raise ValueError("Latitude must be between -90 and 90")
+
+        if "longitude" in place_data and not (-180 <= place_data["longitude"] <= 180):
+            raise ValueError("Longitude must be between -180 and 180")
+
+        if "amenities" in place_data:
+            for aid in place_data["amenities"]:
+                if not self.amenity_repo.get(aid):
+                    raise ValueError(f"Amenity {aid} does not exist")
+
         for key, value in place_data.items():
             if hasattr(place, key):
                 setattr(place, key, value)
+
         self.place_repo.update(place_id, place)
         return place
 
@@ -113,7 +163,8 @@ class HBnBFacade:
     def get_reviews_by_place(self, place_id):
         return [
             review for review in self.review_repo.get_all()
-            if review.place_id == place_id]
+            if review.place_id == place_id
+        ]
 
     def update_review(self, review_id, review_data):
         review = self.review_repo.get(review_id)
@@ -124,3 +175,4 @@ class HBnBFacade:
                 setattr(review, key, value)
         self.review_repo.update(review_id, review)
         return review
+
