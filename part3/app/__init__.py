a496a446cf9d820181577fce8f7092c264bbc421
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-15 15:28:54 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,22 +1,16 @@
+# app/__init__.py
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
-
-# Extensions
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-jwt = JWTManager()
+from app.extensions import db, bcrypt, jwt
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hbnb.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'supersecretkey'  # يمكنك تغييره
+    app.config['SECRET_KEY'] = 'supersecretkey'
 
-    # Init extensions
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
 
     return app
+
