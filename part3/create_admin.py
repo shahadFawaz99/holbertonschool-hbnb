f5d95c0d3a431dd0309ffc6aaa9533f471ad5c1f
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-15 15:35:02 +0300

    Update create_admin.py

--- part3/create_admin.py
+++ part3/create_admin.py
@@ -1,15 +1,21 @@
+# create_admin.py
 from app import create_app
 from app.extensions import db
 from app.models.user import User
 
 app = create_app()
+
 with app.app_context():
-    user = User(
-        email="admin@example.com",
-        is_admin=True
-    )
-    user.hash_password("admin123")
-    db.session.add(user)
-    db.session.commit()
-    print("✅ Admin user created: admin@example.com / admin123")
+    existing_admin = User.query.filter_by(email="admin@example.com").first()
+    if existing_admin:
+        print("⚠️ Admin user already exists.")
+    else:
+        admin_user = User(
+            email="admin@example.com",
+            is_admin=True
+        )
+        admin_user.hash_password("admin123")  # كلمة المرور
+        db.session.add(admin_user)
+        db.session.commit()
+        print("✅ Admin user created: admin@example.com / admin123")
 
