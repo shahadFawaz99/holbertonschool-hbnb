a241aacfa0319c6ca91802fff2ede4680cced12e
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-16 04:17:26 +0300

    Update create_admin.py

--- part3/create_admin.py
+++ part3/create_admin.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-
 from app import create_app, db
 from app.models.user import User
 
 app = create_app()
 
 with app.app_context():
-    # إنشاء جميع الجداول إذا لم تكن موجودة
-    db.create_all()
+    db.create_all()  
 
-    # تحقق إذا كان admin موجود
-    existing_admin = User.query.filter_by(email="admin@example.com").first()
-    if not existing_admin:
-        admin = User(
-            email="admin@example.com",
-            is_admin=True
-        )
-        admin.hash_password("admin123")  # ضع كلمة المرور
+    if not User.query.filter_by(email="admin@example.com").first():
+        admin = User(email="admin@example.com", is_admin=True)
+        admin.hash_password("admin123")
         db.session.add(admin)
         db.session.commit()
         print("✅ Admin created")
     else:
         print("ℹ️ Admin already exists")
+
+
 
 
