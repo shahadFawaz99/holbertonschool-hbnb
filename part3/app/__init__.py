9888e02ce9c84be3d6e2d760ff0a6f2edb5e0f72
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-16 04:14:40 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,6 +1,11 @@
-# app/__init__.py
 from flask import Flask
-from app.extensions import db, bcrypt, jwt
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +16,9 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    from app.routes.auth import auth_bp
+    app.register_blueprint(auth_bp)
 
     return app
 
